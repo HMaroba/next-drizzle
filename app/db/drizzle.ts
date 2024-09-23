8ebc301726e1aa91511f4427be0b9e77bed62286
HMaroba <marobahlalele910@gmail.com>
2024-09-23 12:46:33 +0200

    Created tables

--- app/db/drizzle.ts
+++ app/db/drizzle.ts
@@ -8,6 +8,8 @@
 
 // export const db = drizzle(sql);
 
+// Postgres below
+
 import { config } from "dotenv";
 import { Pool } from 'pg'; // Import pg module
 import { drizzle } from 'drizzle-orm/node-postgres'; 
@@ -21,3 +23,18 @@ const pool = new Pool({
 
 // Use drizzle with the local Postgres connection
 export const db = drizzle(pool); // Pass the pool instance
+
+// import { config } from "dotenv";
+// import mysql from 'mysql2/promise'; // Import MySQL client
+// import { drizzle } from 'drizzle-orm/mysql2'; // Import drizzle ORM for MySQL
+
+// config({ path: ".env" }); // Load environment variables from .env
+
+// // Create a connection pool for MySQL
+// const pool = mysql.createPool({
+//   uri: process.env.DATABASE_URL, // Use the connection string from .env
+// });
+
+// // Use drizzle ORM with MySQL connection
+// export const db = drizzle(pool); // Pass the MySQL connection pool to drizzle
+
