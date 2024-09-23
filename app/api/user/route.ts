a9357bffc4146140dd4a1baea5af0b304a118867
HMaroba <marobahlalele910@gmail.com>
2024-09-23 16:00:50 +0200

    Created users api

---
+++ app/api/user/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "../../db/drizzle";
+import { users } from "../../db/schema";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { id, name, phone } = await req.json();
+
+    const newUser = await db.insert(users).values({
+      id: id,
+      name: name,
+      phone: phone,
+    });
+    
+    return NextResponse.json({
+      status: 201,
+      user: newUser,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: "Server error" + error,
+    });
+  }
+}
