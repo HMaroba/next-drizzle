be5f19b26841d751c9f8e1d38031ecfe5d80b885
HMaroba <marobahlalele910@gmail.com>
2024-09-23 15:11:56 +0200

    Created another tables

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,8 +1,22 @@
-import { integer, text, boolean, pgTable } from "drizzle-orm/pg-core";
+import { integer, text, boolean, pgTable, numeric } from "drizzle-orm/pg-core";
 
 export const todo = pgTable("todo", {
   id: integer("id").primaryKey(),
   title: text("title").notNull(),
   description: text("description").notNull(),
   done: boolean("done").default(false).notNull(),
+});
+
+export const users = pgTable("users", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  phone: numeric("phone").notNull(),
+});
+
+export const pets = pgTable("pets", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  ownerId: integer("owner_id")
+    .notNull()
+    .references(() => users.id),
 });
