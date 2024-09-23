c964a38e802dd6f758ed337c3b4db64b89069f48
HMaroba <marobahlalele910@gmail.com>
2024-09-23 12:25:48 +0200

    Created drizzle connect

--- app/db/drizzle.ts
+++ app/db/drizzle.ts
@@ -1,9 +1,23 @@
+// import { config } from "dotenv";
+// import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-http';
+
+// config({ path: ".env" }); // or .env.local
+
+// const sql = neon(process.env.DATABASE_URL!);
+
+// export const db = drizzle(sql);
+
 import { config } from "dotenv";
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { Pool } from 'pg'; // Import pg module
+import { drizzle } from 'drizzle-orm/node-postgres'; 
 
-config({ path: ".env" }); // or .env.local
+config({ path: ".env" }); // Load .env variables
 
-const sql = neon(process.env.DATABASE_URL!);
+// Create a new Pool instance to connect to the local database
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-export const db = drizzle(sql);
+// Use drizzle with the local Postgres connection
+export const db = drizzle(pool); // Pass the pool instance
